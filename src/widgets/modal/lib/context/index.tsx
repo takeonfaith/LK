440731b472e4a6d8e0a6fe12e0170f54644ab65f
Maxim725 <maks_10000@bk.ru>
2022-01-06 20:44:18 +0300

    [upgrade] добавление нового функционала

--- src/widgets/modal/lib/context/index.tsx
+++ src/widgets/modal/lib/context/index.tsx
@@ -1,16 +1,22 @@
-import React, { useState, useCallback, createContext } from 'react'
+import React, { useState, useCallback, createContext, useMemo } from 'react'
 
 export const Context = createContext<ModalContext>({
     isOpen: false,
     component: undefined,
     toggle: () => {},
+    back: () => {},
+    close: () => {},
+    open: () => {},
 })
 
 export interface ModalContext {
     isOpen: boolean
     component: React.ReactElement<any, any> | undefined
     toggle: (event?: any) => void
-    setComponent?: React.Dispatch<React.SetStateAction<React.ReactElement<any, any> | undefined>>
+    setComponent?: (Component: React.ReactElement<any, any> | undefined) => void
+    back: () => void
+    open: (Component: React.ReactElement<any, any> | undefined) => void
+    close: () => void
 }
 
 interface Props {
@@ -19,15 +25,53 @@ interface Props {
 
 export const ModalProvider = ({ children }: Props) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
-    const [component, setComponent] = useState<React.ReactElement<any, any>>()
+    const [openModals, setOpenModals] = useState<React.ReactElement<any, any>[]>([])
+
+    const canBack = openModals.length > 0
 
     const toggle = useCallback(() => {
+        if (isOpen) {
+            setOpenModals([])
+        }
         setIsOpen((prev) => !prev)
     }, [setIsOpen])
+
+    const back = useCallback(() => {
+        if (canBack) {
+            openModals.pop()
+            setOpenModals([...openModals])
+        }
+    }, [openModals, setOpenModals])
+
+    const open = useCallback(
+        (Component: React.ReactElement<any, any> | undefined) => {
+            if (Component) {
+                setOpenModals([...openModals, Component])
+                setIsOpen(() => true)
+            }
+        },
+        [setOpenModals, setIsOpen],
+    )
+
+    const close = useCallback(() => {
+        setOpenModals([])
+        setIsOpen(() => false)
+    }, [setOpenModals, setIsOpen])
+
+    const component = useMemo(() => openModals[openModals.length - 1], [openModals])
+
+    const setComponent = useCallback((Component: React.ReactElement<any, any> | undefined) => {
+        if (Component) {
+            setOpenModals([...openModals, Component])
+        }
+    }, [])
 
     return (
         <Context.Provider
             value={{
+                back,
+                open,
+                close,
                 isOpen,
                 toggle,
                 component,
