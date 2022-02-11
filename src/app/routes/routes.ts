bd5160ae52541de7c5c6dc524eb41bc98559dc81
Konstantin Doloz <takeonfaith6@gmail.com>
2022-02-11 10:42:31 +0300

    payments-removed

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -15,7 +15,7 @@ import { IconType } from 'react-icons/lib'
 import AcadPerformance from '../../pages/acad-performance'
 import Home from '../../pages/home'
 import LoginPage from '../../pages/login'
-import PaymentsPage from '../../pages/payments'
+// import PaymentsPage from '../../pages/payments'
 import SchedulePage from '../../pages/schedule'
 
 export const HOME_ROUTE = '/home'
@@ -102,7 +102,7 @@ export const privateRoutes: IRoutes = {
         title: 'Оплата',
         icon: FiDollarSign,
         path: PAYMENTS_ROUTE,
-        Component: PaymentsPage,
+        Component: () => PageIsNotReady({ oldVersionUrl: '/payments' }),
         isTemplate: false,
     },
     6: {
