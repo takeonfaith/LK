403ade49462baee5cb29c8ce37e07df7b379c6cc
Konstantin Doloz <takeonfaith6@gmail.com>
2022-06-01 15:21:45 +0300

    instructions page

--- src/shared/ui/atoms/center-page.tsx
+++ src/shared/ui/atoms/center-page.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
-const CenterPage = styled.div`
+const CenterPage = styled.div<{ alignItems?: string; padding?: string }>`
     display: flex;
-    align-items: center;
+    align-items: ${({ alignItems }) => alignItems ?? 'center'};
     justify-content: center;
     height: 100%;
     color: var(--text);
+    padding: ${({ padding }) => padding};
 `
 
 export default CenterPage
