35362dc92029ca79c0c89cfcb9816b4405a68000
skael <nikkarpa@yandex.ru>
2022-08-29 00:49:04 +0300

    rollback applications

--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
-import { OLD_LK_URL } from '@consts'
-import { Error } from '@ui/error'
-import { LinkButton } from '@ui/atoms'
-// import { TemplateApplications } from 'widgets'
+import { TemplateApplications } from 'widgets'
 
 const ApplicationsPage = () => {
-    return (
-        <Error text={'Cервис временно недоступен. для того, чтобы воспользоваться им, перейдите на старую версию'}>
-            <LinkButton
-                text={'Перейти к версии в старом дизайне'}
-                background="var(--purple)"
-                width="300px"
-                href={`${OLD_LK_URL}/?p=sprav`}
-            />
-        </Error>
-    )
-    // return <TemplateApplications isTeachers={false} />
+    return <TemplateApplications isTeachers={false} />
 }
 
 export default ApplicationsPage
