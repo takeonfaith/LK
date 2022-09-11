15efb85a5864af293a209616c6d01a63bd0a2fba
Надя <nk20001000@gmail.com>
2022-09-11 23:39:46 +0300

    add info

--- src/pages/payments/pages/education/index.tsx
+++ src/pages/payments/pages/education/index.tsx
@@ -8,11 +8,11 @@ import {
     PaymentsInfo,
 } from '@features/payments'
 import getEducationPaygraphColumns from '@pages/payments/lib/get-education-paygraph-columns'
-import { Divider, LinkButton, Title } from '@ui/atoms'
+import { Divider, LinkButton, Message, Title } from '@ui/atoms'
 import Block from '@ui/block'
 import getCorrectNumberFormat from '@utils/get-correct-number-format'
 import React from 'react'
-import { FiDownload } from 'react-icons/fi'
+import { FiDownload, FiInfo } from 'react-icons/fi'
 
 const EducationPayments = () => {
     const { data } = paymentsModel.selectors.usePayments()
@@ -24,6 +24,10 @@ const EducationPayments = () => {
             {data?.education.map((education, i) => {
                 return (
                     <React.Fragment key={education.number}>
+                    <Message type="info" title="Информация" icon={<FiInfo />}>
+                    По возникновении технических проблем при подписании договоров и дополнительных соглашений
+                    в Личном кабинете просим обращаться на почту <a href='mailto:info@mospolytech.ru'>info@mospolytech.ru</a>
+                    </Message>
                         <div className="blocks-wrapper" key={i}>
                             <Block orientation="vertical" maxWidth="800px">
                                 <Title size={2} align="left" bottomGap>
@@ -78,6 +82,9 @@ const EducationPayments = () => {
                                     <Title size={2} align="left" bottomGap>
                                         Доп. соглашение
                                     </Title>
+                                    <Message type="info" title="Информация" icon={<FiInfo />}>
+                                    Подписание дополнительных соглашений к 3-сторонним договорам в Личном кабинете не предусмотрено
+                                    </Message>
                                     <ElectronicAgreementList
                                         electronicAgreements={education.agreements.filter(
                                             (item) => new Date(item?.date) > new Date('2022-02-01'),
