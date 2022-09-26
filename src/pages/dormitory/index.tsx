d701ad4554e7f6baf93b278cad216fe88a849301
mserygin <matvey@serygin.ru>
2022-09-26 21:35:49 +0300

    waiting list in the hostel has been changed

--- src/pages/dormitory/index.tsx
+++ src/pages/dormitory/index.tsx
@@ -20,13 +20,15 @@ const DormitoryPage = () => {
                 </Title>
                 <Message title="Уважаемые студенты!" type="info">
                     <p>
-                        Для удобства Для удобстваДля удобстваДля удобстваДля удобстваДля удобстваДля удобстваДля
-                        удобстваДля удобстваДля удобстваДля удобстваДля удобстваДля удобства.
+                        В данном разделе представлен порядок предоставления мест в студенческом городке Московского
+                        городке Московского Политеха с указанием актуального списка нуждающихся в общежитии.
                     </p>
                 </Message>
                 <FileLink
                     type={'document'}
-                    link={'https://e.mospolytech.ru/old/storage/files/matpom_docs.pdf'}
+                    link={
+                        'https://vk.com/away.php?to=https%3A%2F%2Fe.mospolytech.ru%2Fold%2Fstorage%2Ffiles%2FDormitory_actual_list.pdf&cc_key='
+                    }
                     title={'Актуальный список'}
                 />
             </Block>
