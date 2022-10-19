76566f0e6835a523b63e6e956bfa792cc6708244
Nikita Karpenko <nikkarpa@yandex.ru>
2022-10-19 13:22:57 +0300

    fix bugs

--- src/shared/lib/get-letters-colors.ts
+++ src/shared/lib/get-letters-colors.ts
@@ -5,7 +5,7 @@ const getLettersColors = (fullName: string) => {
     const firstLetters = getNameFirstLetters(fullName)
     const color: IColorPalette = Colors[letterColorMatch[firstLetters[0]]]
 
-    return color.main
+    return color?.main
 }
 
 export default getLettersColors
