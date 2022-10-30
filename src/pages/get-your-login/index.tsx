60010fcae35c7c6c20e821780ff830e037841930
Nikita Karpenko <nikkarpa@yandex.ru>
2022-10-30 20:17:42 +0300

    fix get login fail

--- src/pages/get-your-login/index.tsx
+++ src/pages/get-your-login/index.tsx
@@ -5,6 +5,8 @@ import React, { useState } from 'react'
 import useTheme from '@utils/hooks/use-theme'
 import { userModel } from '@entities/user'
 import styled from 'styled-components'
+import { useStore } from 'effector-react'
+import { USER_NOT_FOUND } from './config/ad-result-status'
 
 const ADNameElement = styled.span`
     display: flex;
@@ -17,15 +19,14 @@ const GetYourLoginPage = () => {
     const [fio, setFio] = useState('')
     const [passportNumber, setPassportNumber] = useState('')
     useTheme()
-    const [loading, setLoading] = useState(false)
+    const loading = useStore(userModel.effects.getLoginEuzFx.pending)
     const [completed, setCompleted] = useState(false)
     const {
         data: { loginEuz },
     } = userModel.selectors.useUser()
 
     const getADName = () => {
-        setLoading(true)
-        userModel.effects.getLoginEuzFx({ fio: fio, pn: passportNumber }).then(() => setLoading(false))
+        userModel.effects.getLoginEuzFx({ fio: fio, pn: passportNumber })
     }
 
     const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -68,6 +69,7 @@ const GetYourLoginPage = () => {
                     Для того, чтобы узнать свой логин единой учетной записи, укажите ваши ФИО полностью (например,
                     Иванов Петр Иванович) и 4 последние цифры номера паспорта:
                 </Message>
+
                 <Input value={fio} setValue={setFio} title="ФИО полностью" placeholder="Иванов Иван Иванович" />
                 <Input
                     value={passportNumber}
@@ -85,6 +87,9 @@ const GetYourLoginPage = () => {
                     isActive={!!fio.length && passportNumber.length === 4}
                 />
                 {!!loginEuz && getADNameElements().map((el, i) => <React.Fragment key={i}>{el}</React.Fragment>)}
+                {!!loginEuz && loginEuz === USER_NOT_FOUND && (
+                    <Message type="failure" title="Не удалось найти пользователя" />
+                )}
             </BlockWrapper>
         </CenterPage>
     )
