5bc5c22f2e871d8585a911faf365b3cd0872756f
Nikita Karpenko <nikkarpa@yandex.ru>
2022-12-02 12:45:33 +0300

    fix now

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,25 +11,6 @@ const Background = styled.div`
 `
 
 const App = () => {
-    useEffect(() => {
-        self.addEventListener('activate', function (event: any) {
-            event.waitUntil(
-                caches.keys().then(function (cacheNames) {
-                    return Promise.all(
-                        cacheNames
-                            .filter(function (cacheName) {
-                                // Return true if you want to remove this cache,
-                                // but remember that caches are shared across
-                                // the whole origin
-                            })
-                            .map(function (cacheName) {
-                                return caches.delete(cacheName)
-                            }),
-                    )
-                }),
-            )
-        })
-    }, [])
     return (
         <ModalProvider>
             <HashRouter basename="/">
