d18bdca78bb51a71d81341266474ee1574f6cffe
Nikita Karpenko <nikkarpa@gmail.com>
2022-12-20 00:33:32 +0300

    fix

--- src/pages/helpful-information/config/pages-config.ts
+++ src/pages/helpful-information/config/pages-config.ts
@@ -119,59 +119,59 @@ const pages: HelpfulPages = [
     },
     {
         title: 'Безопасность',
-        visible: 'student',
+        visible: 'all',
         content: [
             {
                 title: 'Безопасность',
                 links: [
                     {
                         title: 'Если оказались на территории вуза в момент наступившей чрезвычайной ситуации',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_2_Esli_okazalis_na_territorii_vuza_v_moment_nastupivshej_chrezvychajnoj_situatsii.pdf',
                     },
                     {
                         title: 'Памятка «Подозрительный предмет»',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_3_Podozritelnyj_predmet.pdf',
                     },
                     {
                         title: 'Памятка студента по действиям в экстремальных ситуациях',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_4_Pamyatka_studenta_po_dejstviyam_v_ekstremalnyh_situatsiyah.pdf',
                     },
                     {
                         title: 'Алгоритм действий при возникновении паники в толпе или в месте массового пребывания людей',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_5_Algoritm_dejstvij_pri_vozniknovenii_paniki_v_tolpe_ili_v_meste_massovogo_prebyvaniya_ljudej.PDF',
                     },
                     {
                         title: 'Безопасность в экстремальных и чрезвычайных ситуациях',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_6_Bezopasnost_v_ekstremalnyh_i_chrezvychajnyh_situatsiyah.PDF',
                     },
                     {
                         title: 'Как понять, что материал экстремистский, и что с этим делать',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_7_Kak_ponyat__chto_material_ekstremistskij__i_chto_s_etim_delat.PDF',
                     },
                     {
                         title: 'Последствия и ответственность за ложное сообщение об акте терроризма',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_8_Posledstviya_i_otvetstvennost_za_lozhnoe_soobschenie_ob_akte_terrorizma.PDF',
                     },
                     {
                         title: 'Последствия участия в несогласованных митингах и протестных акциях',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_9_Posledstviya_uchastiya_v_nesoglasovannyh_mitingah_i_protestnyh_aktsiyah.PDF',
                     },
                     {
                         title: 'Признаки вербовки в экстремистскую организацию и как с этим справиться',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_10_Priznaki_verbovki_v_ekstremistskuju_organizatsiju_i_kak_s_etim_spravitsya.PDF',
                     },
                     {
                         title: 'Что делать, если вы обнаружили бесхозный предмет',
-                        visible: 'student',
+                        visible: 'all',
                         href: 'https://e.mospolytech.ru/old/storage/files/notes/Metodicheskie_ukazaniya_i_pamyatki_11_Chto_delat__esli_vy_obnaruzhili_beshoznyj_predmet.PDF',
                     },
                 ],
