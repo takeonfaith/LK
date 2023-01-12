a2c76f7b887a6a044265f7b193c32ca8133d9073
Nikita Karpenko <nikkarpa@gmail.com>
2023-01-13 00:05:11 +0300

    add medical cert?

--- src/pages/medical-certificate/index.tsx
+++ src/pages/medical-certificate/index.tsx
@@ -1,3 +1,4 @@
+import { applicationsModel } from '@entities/applications'
 import BaseApplicationWrapper from '@pages/applications/ui/base-application-wrapper'
 import { FormBlock, SubmitButton } from '@shared/ui/atoms'
 import InputArea from '@shared/ui/input-area'
@@ -10,10 +11,45 @@ const MedicalCertificate = () => {
     const [loading, setLoading] = useState(false)
     const isDone = completed ?? false
 
+    const {
+        data: { dataUserApplication },
+    } = applicationsModel.selectors.useApplications()
+
     useEffect(() => {
         setForm({
             title: 'Загрузите медицинскую справку',
-            data: [],
+            data: [
+                {
+                    title: 'ФИО',
+                    type: 'text',
+                    fieldName: 'name',
+                    required: true,
+                    value:
+                        dataUserApplication?.surname +
+                        ' ' +
+                        dataUserApplication?.name +
+                        ' ' +
+                        dataUserApplication?.patronymic,
+                },
+                {
+                    title: 'Телефон',
+                    type: 'tel',
+                    mask: true,
+                    fieldName: 'phone',
+                    editable: true,
+                    required: true,
+                    value: dataUserApplication?.phone ?? '',
+                },
+
+                {
+                    title: 'Email',
+                    type: 'email',
+                    fieldName: 'email',
+                    value: dataUserApplication?.email ?? '',
+                    editable: true,
+                    required: true,
+                },
+            ],
             documents: { files: [], fieldName: 'docs', required: true, maxFiles: 1 },
         })
     }, [])
