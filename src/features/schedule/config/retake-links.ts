13fa4b3d8c06ca3d8a4ec0865ead95aec5f68497
kurakinasv <svetakurakina2002@gmail.com>
2023-02-27 11:42:51 +0300

    retake schedule links update

--- src/features/schedule/config/retake-links.ts
+++ src/features/schedule/config/retake-links.ts
@@ -3,100 +3,88 @@ import { RetakeLink } from '../types/retake-types'
 const RETAKE_LINKS: Record<string, RetakeLink> = {
     feiu: {
         fullTitle: 'Факультет экономики и управления',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FEU.zip?2',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FEU.zip?5',
         links: [],
     },
     fuigh: {
         fullTitle: 'Факультет урбанистики и городского хозяйства',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FUiGH.zip',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FUiGH.zip?1',
         links: [],
     },
     fbk: {
         fullTitle: 'Факультет базовых компетенций',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FBK.zip?4',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FBK.zip?8',
         links: [],
     },
     fhtib: {
         fullTitle: 'Факультет химической технологии и биотехнологии',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FHTiB.zip?1',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FHTiB.zip?2',
         links: [],
     },
     tf: {
         fullTitle: 'Транспортный факультет',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/TF.zip?1',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/TF.zip?7',
         links: [],
     },
     fm: {
         fullTitle: 'Факультет машиностроения',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FM.zip?1',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/FM.zip?6',
         links: [],
     },
     fit: {
         fullTitle: 'Факультет информационных технологий',
         links: [
             {
-                title: 'ФИТ 2021-2022-лето, кафедра ИБ, бак+спец',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_IB__bakalavry_i_spetsialisty.pdf',
+                title: 'ФИТ 2022-2023-зима, кафедра ИБ',
+                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2022-2023-zima__kafedra_IB.pdf',
             },
             {
-                title: 'ФИТ 2021-2022-лето, кафедра ПИ',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_PI.pdf',
+                title: 'ФИТ 2022-2023-зима, кафедра ИКТ',
+                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2022-2023-zima__kafedra_IKT.pdf',
             },
             {
-                title: 'ФИТ 2021-2022-лето, кафедра СМАРТ, профиль САПР',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_SMART__profil_SAPR.pdf',
-            },
-            {
-                title: 'ФИТ 2021-2022-лето, кафедра СМАРТ, магистры',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_SMART__magistry.pdf',
-            },
-            {
-                title: 'ФИТ 2021-2022-лето, кафедра СМАРТ, профиль КФС',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_SMART__profil_KFS.pdf',
-            },
-            {
-                title: 'ФИТ 2021-2022-лето, кафедра ИКТ, бакалавры и магистры',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_IKT__bakalavry_i_magistry.pdf',
+                title: 'ФИТ 2022-2023-зима, кафедра ПИ',
+                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2022-2023-zima__kafedra_PI.pdf',
             },
             {
                 title: 'ФИТ 2021-2022-лето, кафедра ИиИТ',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021-2022-leto__kafedra_IiIT.pdf',
+                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2022-2023-zima__kafedra_IiIT.pdf',
             },
             {
                 title: 'ФИТ 2021-2022-лето, кафедра ИиИТ, ВНЕШНИЕ',
-                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2021_2022_leto__kafedra_IiIT__VNEShNIE.pdf',
+                href: 'https://e.mospolytech.ru/old/storage/files/arrears/FIT_2022-2023-zima__kafedra_IiIT_VNEShNIE.pdf',
             },
         ],
         href: '',
     },
     pi: {
         fullTitle: 'Полиграфический институт',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/PI.zip?2',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/PI.zip?4',
         links: [],
     },
     iidizh: {
         fullTitle: 'Институт издательского дела и журналистики',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/IIDiZh.zip?1',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/IIDiZh.zip?2',
         links: [],
     },
     igrik: {
         fullTitle: 'Институт графики и искусства книги имени В.А.Фаворского',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/IGRIK.zip?1',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/IGRIK.zip?2',
+        links: [],
+    },
+    pishe: {
+        fullTitle: 'Передовая инженерная школа электротранспорта',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/PIShE.zip',
         links: [],
     },
     cpd: {
         fullTitle: 'Центр проектной деятельности',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/TsPD.zip?2',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/TsPD.zip?3',
         links: [],
     },
     nid: {
         fullTitle: 'График приема задолженностей по НИД (аспиранты)',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/Grafik_priema_zadolzhennostej_po_NID_(aspiranty).doc?1',
-        links: [],
-    },
-    nivk: {
-        fullTitle: 'График приема задолженностей по научным исследования выпускного курса (аспиранты)',
-        href: 'https://e.mospolytech.ru/old/storage/files/arrears/Grafik_priema_zadolzhennostej_po_nauchnym_issledovaniya_vypusknogo_kursa_(aspiranty).doc',
+        href: 'https://e.mospolytech.ru/old/storage/files/arrears/Grafik_priema_zadolzhennostej_po_NID_(aspiranty).doc',
         links: [],
     },
 }
