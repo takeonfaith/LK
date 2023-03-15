5b1ffbce699dc2312b608c2c358e1f1e3d1505d0
matvey <matvey@serygin.ru>
2023-03-15 13:34:57 +0300

    new element for step_by-step-form

---
+++ src/features/applications/ui/molecules/step-by-step-form.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const stepByStepForm = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default stepByStepForm
