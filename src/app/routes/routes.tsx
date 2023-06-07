e6880288d0339a6a1590085a54c9565c6c51c5d4
Nikita Karpenko <nakarpenko@sbercloud.ru>
2023-06-07 23:28:39 +0300

    disable zoom and add payment notify

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -359,7 +359,7 @@ export const hiddenRoutes: () => IRoutes = () => ({
         title: 'Переселение в другое общежитие',
         icon: BiIdCard,
         path: RELOCATION_TO_ANOTHER_HOSTEL,
-        Component: isProduction ? ApplicationRedirect : RelocationToAnotherHostelPage,
+        Component: RelocationToAnotherHostelPage,
         color: 'blue',
         isTemplate: false,
         isSubPage: true,
--- src/widgets/header/ui/molecules/settings-button.tsx
+++ src/widgets/header/ui/molecules/settings-button.tsx
@@ -2,20 +2,22 @@ import { FiSettings } from 'react-icons/fi'
 
 import { Button } from '@ui/button'
 import React from 'react'
-import { useModal } from 'widgets/modal/lib'
+import { Link } from 'react-router-dom'
+import { SETTINGS_ROUTE } from '@app/routes/general-routes'
+import { contextMenuModel } from '@entities/context-menu'
 
 const SettingsButton = () => {
-    const { open } = useModal()
-
     return (
-        <Button
-            text="Настройки"
-            icon={<FiSettings />}
-            onClick={() => open(<></>)}
-            width="100%"
-            align="left"
-            background="var(--schedule)"
-        />
+        <Link to={SETTINGS_ROUTE}>
+            <Button
+                text="Настройки"
+                icon={<FiSettings />}
+                onClick={() => contextMenuModel.events.close()}
+                width="100%"
+                align="left"
+                background="var(--schedule)"
+            />
+        </Link>
     )
 }
 
