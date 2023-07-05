78aab7d61057c85054e1063e409e2ae439919728
skaele <nikkarpa@yandex.ru>
2023-07-06 00:10:23 +0300

    add connection to server

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,10 @@ import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig((conf) => {
-    var outDir = conf.mode === 'development' ? 'dist' : 'build'
     return {
         server: {
             open: true,
-            port: 3000
+            port: 80,
         },
         preview: { port: 3000 },
         plugins: [
@@ -20,7 +19,7 @@ export default defineConfig((conf) => {
             checker({ typescript: true, eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' } }),
         ],
         build: {
-            outDir,
+            outDir: 'build',
             manifest: true,
         },
         esbuild: {
