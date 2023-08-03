64ea7f22ff8a830b46c3ca33d6814d6b24d89d6f
awqsomee <awqsome@bk.ru>
2023-08-03 14:13:13 +0300

    Revert "Fix wrong ternar operator"
    This reverts commit af72ffb676faf518ec8ae6ca48ffac2cbffa91c2.

--- src/shared/lib/find-semestr.ts
+++ src/shared/lib/find-semestr.ts
@@ -1,7 +1,7 @@
 const findSemestr = (date: string, course: number | string) => {
     const month = new Date(date).getMonth()
 
-    return month > 1 && month < 7 ? +course * 2 - 1 : +course * 2
+    return month > 1 && month < 7 ? +course * 2 : +course * 2 - 1
 }
 
 export default findSemestr
