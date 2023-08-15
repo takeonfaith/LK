e6b5a91a5fb5a1de6265bcf6786b2086f028372c
awqsomee <awqsome@bk.ru>
2023-08-15 18:31:25 +0300

    update dateTime info for superior room

--- src/pages/application-for-superior-room/lib/get-status.ts
+++ src/pages/application-for-superior-room/lib/get-status.ts
@@ -1,13 +1,13 @@
 import { User } from '@api/model'
 
 const getStatusFormSuperiorRoom = (user: User) => {
-    const StartDateSuperiorRoom = 'Aug 15 2023 00:00:00 GMT+0300'
-    const EndDateSuperiorRoom = 'Aug 16 2023 00:00:00 GMT+0300'
+    const StartDateSuperiorRoom = 'Aug 17 2023 12:00:00 GMT+0300'
+    const EndDateSuperiorRoom = 'Aug 19 2023 12:00:00 GMT+0300'
     const currentTime = new Date()
 
     if (user.enterYear !== '2023/2024') return 'Форма доступна только для студентов 1 курса'
 
-    if (currentTime < new Date(StartDateSuperiorRoom)) return 'Подача заявок открыта с 15 августа до  16 августа!'
+    if (currentTime < new Date(StartDateSuperiorRoom)) return 'Подача заявок открыта с 17 августа до 19 августа!'
 
     if (currentTime > new Date(EndDateSuperiorRoom)) return 'Подача заявок закрыта'
 
