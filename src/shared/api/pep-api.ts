2d84d3ed61853f65c341f0f76f801668c8bc1551
awqsomee <awqsome@bk.ru>
2023-08-22 09:56:31 +0300

    changed emulation to api

--- src/shared/api/pep-api.ts
+++ src/shared/api/pep-api.ts
@@ -1,13 +1,11 @@
 import { $api } from './config'
 import token from '@utils/token'
 import { ElectronicInteraction } from './model'
-import emulateRequest from '@shared/lib/emulate-request'
 
 export const get = async () => {
     return (await $api.get<ElectronicInteraction[]>(`?getPEPStatus&token=${token()}`)).data
 }
 
 export const set = async () => {
-    // return (await $api.get(`?setPEPAccept&token=${token()}`)).data
-    return await emulateRequest([{ result: 'ok' }])
+    return (await $api.get(`?setPEPAccept&token=${token()}`)).data
 }
