b66a936d4bc2d3e47493e32658dc447ed9f34bdc
awqsomee <awqsome@bk.ru>
2023-08-25 20:15:27 +0300

    change document links to polytech server

--- src/pages/onboarding/lib/Documents.tsx
+++ src/pages/onboarding/lib/Documents.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 export const Documents = () => (
     <>
-        <a href="https://disk.yandex.ru/i/40qk8-JKl4rMqg" target="_blank" rel="noreferrer">
+        <a
+            href="https://e.mospolytech.ru/old/storage/files/Kollektivnyj_dogovor_na_2020-2023_gg.pdf"
+            target="_blank"
+            rel="noreferrer"
+        >
             Коллективный договор на 2020-2023 гг.
         </a>
-        <a href="https://disk.yandex.ru/i/V6Moz3nh-5THvQ" target="_blank" rel="noreferrer">
+        <a
+            href="https://e.mospolytech.ru/old/storage/files/Lokalnye_normativnye_akty__regulirujuschie_sotsialno-trudovye_otnosheniya.pdf"
+            target="_blank"
+            rel="noreferrer"
+        >
             Локальные нормативные акты, регулирующие социально-трудовые отношения
         </a>
     </>
