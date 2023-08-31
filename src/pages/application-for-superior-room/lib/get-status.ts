55d2680f7753c485079f636aa82a1216c03786f4
awqsomee <awqsome@bk.ru>
2023-08-31 18:50:50 +0300

    turn off course check

--- src/pages/application-for-superior-room/lib/get-status.ts
+++ src/pages/application-for-superior-room/lib/get-status.ts
@@ -1,11 +1,12 @@
 import { User } from '@api/model'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getStatusFormSuperiorRoom = (user: User) => {
     const StartDateSuperiorRoom = 'Aug 17 2023 12:00:00 GMT+0300'
     const EndDateSuperiorRoom = 'Aug 19 2023 12:00:00 GMT+0300'
     const currentTime = new Date()
 
-    if (user.enterYear !== '2023/2024') return 'Форма доступна только для студентов 1 курса'
+    // if (user.enterYear !== '2023/2024') return 'Форма доступна только для студентов 1 курса'
 
     if (currentTime < new Date(StartDateSuperiorRoom)) return 'Подача заявок открыта с 17 августа до 19 августа!'
 
