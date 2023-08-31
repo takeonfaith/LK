4e195cd5df1e09cd8df703eb84b66e93cde0e062
awqsomee <awqsome@bk.ru>
2023-08-31 19:22:08 +0300

    make page available for tests

--- src/pages/application-for-superior-room/lib/get-status.ts
+++ src/pages/application-for-superior-room/lib/get-status.ts
@@ -2,7 +2,7 @@ import { User } from '@api/model'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getStatusFormSuperiorRoom = (user: User) => {
-    const StartDateSuperiorRoom = 'Sep 01 2023 12:00:00 GMT+0300'
+    const StartDateSuperiorRoom = 'Aug 31 2023 12:00:00 GMT+0300'
     const EndDateSuperiorRoom = 'Sep 03 2023 12:00:00 GMT+0300'
     const currentTime = new Date()
 
