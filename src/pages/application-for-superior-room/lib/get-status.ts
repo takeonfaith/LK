55ea3c3faa0ddd5b90b0c7b70e7cbce2212ff491
awqsomee <awqsome@bk.ru>
2023-09-01 09:07:47 +0300

    Close superiour room for release

--- src/pages/application-for-superior-room/lib/get-status.ts
+++ src/pages/application-for-superior-room/lib/get-status.ts
@@ -2,8 +2,8 @@ import { User } from '@api/model'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getStatusFormSuperiorRoom = (user: User) => {
-    const StartDateSuperiorRoom = 'Aug 31 2023 12:00:00 GMT+0300'
-    const EndDateSuperiorRoom = 'Sep 03 2023 12:00:00 GMT+0300'
+    const StartDateSuperiorRoom = 'Aug 17 2023 12:00:00 GMT+0300'
+    const EndDateSuperiorRoom = 'Aug 19 2023 12:00:00 GMT+0300'
     const currentTime = new Date()
 
     // if (user.enterYear !== '2023/2024') return 'Форма доступна только для студентов 1 курса'
