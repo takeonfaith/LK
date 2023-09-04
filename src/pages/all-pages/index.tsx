8526e746050fd8958a1d76644a98a2f906429762
awqsomee <awqsome@bk.ru>
2023-09-04 13:42:41 +0300

    Rename group for students

--- src/pages/all-pages/index.tsx
+++ src/pages/all-pages/index.tsx
@@ -1,5 +1,6 @@
 import { Groups, IRoutes } from '@app/routes/general-routes'
 import { menuModel } from '@entities/menu'
+import { userModel } from '@entities/user'
 import { FoundPages } from '@features/all-pages'
 import getGroupPages, { routesOrder } from '@features/all-pages/lib/get-group-pages'
 import search from '@features/all-pages/lib/search'
@@ -15,6 +16,10 @@ const AllPages = () => {
     const [foundPages, setFoundPages] = useState<IRoutes | null>(null)
     const [searchValue, setSearchValue] = useState<string>('')
     const groupedPages = useMemo(() => getGroupPages(visibleRoutes), [visibleRoutes])
+
+    const {
+        data: { user },
+    } = userModel.selectors.useUser()
 
     if (!visibleRoutes) return null
 
@@ -39,9 +44,15 @@ const AllPages = () => {
                                 })
                                 .map((group) => {
                                     const links = groupedPages[group as Groups]
+                                    if (group === 'Кадровая среда') {
+                                    }
                                     return (
                                         <LinksList
-                                            title={group}
+                                            title={
+                                                group === 'Кадровая среда' && user?.user_status === 'stud'
+                                                    ? 'Договоры и оплаты'
+                                                    : group
+                                            }
                                             key={group}
                                             doNotShow="all"
                                             align="left"
