4a6df519397fbf9e550b391f6f6bbeb235732294
awqsomee <awqsome@bk.ru>
2023-09-08 13:52:48 +0300

    2nd wave dates update

--- src/pages/application-for-superior-room/lib/get-status.ts
+++ src/pages/application-for-superior-room/lib/get-status.ts
@@ -1,7 +1,7 @@
 import { User } from '@api/model'
 
-export const StartDateSuperiorRoom = 'Sep 06 2023 12:00:00 GMT+0300'
-export const EndDateSuperiorRoom = 'Sep 07 2023 12:00:00 GMT+0300'
+export const StartDateSuperiorRoom = 'Sep 07 2023 12:00:00 GMT+0300'
+export const EndDateSuperiorRoom = 'Sep 08 2023 12:00:00 GMT+0300'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getStatusFormSuperiorRoom = (user: User) => {
@@ -10,7 +10,7 @@ export const getStatusFormSuperiorRoom = (user: User) => {
     // if (user.enterYear !== '2023/2024') return 'Форма доступна только для студентов 1 курса'
 
     if (currentTime < new Date(StartDateSuperiorRoom))
-        return 'Подача заявок открыта с 12:00 6 сентября до 12:00 7 сентября!'
+        return 'Подача заявок открыта с 12:00 7 сентября до 12:00 8 сентября!'
 
     if (currentTime > new Date(EndDateSuperiorRoom)) return 'Подача заявок закрыта'
 
