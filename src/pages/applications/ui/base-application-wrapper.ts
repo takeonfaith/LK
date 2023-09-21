2d714f814811c7c9e67ea3da885ef0e98bf6d4bd
awqsomee <awqsome@bk.ru>
2023-09-21 14:52:47 +0300

    fix dumb scroll ish

--- src/pages/applications/ui/base-application-wrapper.ts
+++ src/pages/applications/ui/base-application-wrapper.ts
@@ -5,7 +5,7 @@ const BaseApplicationWrapper = styled.div<{ isDone: boolean }>`
     align-items: ${({ isDone }) => (isDone ? 'center' : 'flex-start')};
     justify-content: center;
     width: 100%;
-    height: ${({ isDone }) => isDone && '100%'};
+    max-height: ${({ isDone }) => isDone && '100%'};
     padding: 10px;
     color: var(--text);
 
