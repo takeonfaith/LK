2347c758e6f7b57e0824d468f2283d7f79dbcee9
awqsomee <awqsome@bk.ru>
2023-09-28 21:43:39 +0300

    dynamic info message

--- src/pages/project-activities/index.tsx
+++ src/pages/project-activities/index.tsx
@@ -1,6 +1,5 @@
 import { projectActivitesModel } from '@entities/project-activites'
 import { Colors } from '@shared/constants'
-import { Message } from '@shared/ui/message'
 import PageBlock from '@shared/ui/page-block'
 import Subtext from '@shared/ui/subtext'
 import { Button, Divider, Title, Wrapper } from '@ui/atoms'
@@ -12,12 +11,15 @@ import CurrentSemestr from './ui/current-semestr'
 import LastSemestr from './ui/last-semestr'
 import InfoModal from './ui/project-info/info-modal'
 import Result from './ui/result'
+import { userModel } from '@entities/user'
+import { ProjectActivityIntroMessage } from './ui/intro-message'
 
 const Container = styled.div`
     h3 {
         background: ${Colors.blue.main};
         background: linear-gradient(to right, ${Colors.blue.main} 0%, ${Colors.pink.main} 100%);
         -webkit-background-clip: text;
+        background-clip: text;
         -webkit-text-fill-color: transparent;
     }
 
@@ -32,6 +34,9 @@ const ProjectStyled = styled.div`
 
 const ProjectActivitiesPage = () => {
     const { data, loading, error } = projectActivitesModel.selectors.useData()
+    const {
+        data: { user },
+    } = userModel.selectors.useUser()
     const { open } = useModal()
 
     const handleOpenModal = () => {
@@ -47,55 +52,7 @@ const ProjectActivitiesPage = () => {
         >
             <Container>
                 <PageBlock>
-                    <Message lineHeight="1.5rem" fontSize="0.86rem" type="info" title="Вниманию студентов 2-4 курсов!">
-                        <p>
-                            Уважаемые студенты!
-                            <br />
-                            <br />
-                            Центр проектной деятельности приглашает вас выбрать проекты, которые вы будете реализовывать
-                            в рамках дисциплины «Проектная деятельность» в 2023-2024 учебном году.
-                            <br />
-                            <br />
-                            Порядок действий для выбора проектов:
-                            <br />
-                            <br />
-                            1. Для знакомства с проектами мы создали для вас:
-                            <br />• Сайт-витрину:{' '}
-                            <a href="http://projects.mospolytech.ru/">http://projects.mospolytech.ru/</a>
-                            <br />• Телеграм-канал, где вы можете задать вопросы инициаторам проектов, общаться с
-                            потенциальными участниками проектов:{' '}
-                            <a href="https://t.me/cpd_projects">https://t.me/cpd_projects</a>
-                            <br />
-                            <br />
-                            2. Регистрация на проекты:
-                            <br />
-                            10 июля в 14:00 регистрация будет открыта студентам с рейтингом 2**;
-                            <br />
-                            12 июля с 14:00 регистрация будет открыта студентам с рейтингом 2** и 1*;
-                            <br />
-                            14 июля с 14:00 регистрация будет открыта всем студентам;
-                            <br />
-                            16 июля в 23:59 регистрация завершится.
-                            <br />
-                            <br />
-                            Подробная информация:{' '}
-                            <a href="https://vk.com/wall-78395495_2038">https://vk.com/wall-78395495_2038</a>
-                            <br />
-                            <br />
-                            <a href="https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSybcuU7Cv0_IGEg8sP7LD_mxQYu3akGUj_xxKX-5gXtdqcwAeDhtWRM8d4WGqscS3_LIQBWUThqoXk/pubhtml?gid=0&single=true">
-                                Расписание занятий по проектной деятельности
-                            </a>
-                            <br />
-                            <br />
-                            <a href="https://docs.google.com/document/u/1/d/e/2PACX-1vRPlG738Pszk13swt9O1EhABnOdze3M39OoZ8rlkSyNae0fbqm8CFjLx21IPA9gZl6qjVLYdAN1gmEx/pub">
-                                Регламент начисления баллов
-                            </a>
-                        </p>
-                    </Message>
-                    {/* {!!user?.id && <Select items={items} selected={selected} setSelected={setSelected} />} */}
-                    {/* <Subtext>
-                        Год: {data?.year} • Полугодие: {data?.semestr}
-                    </Subtext> */}
+                    <ProjectActivityIntroMessage course={user?.course} />
                     <ProjectStyled>
                         <Subtext fontSize="0.85rem">Вам назначен проект</Subtext>
                         <Title size={3} align="left" bottomGap>
