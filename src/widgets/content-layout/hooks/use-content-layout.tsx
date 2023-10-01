80f516864fc367168e8b706c47fe48ed482247e6
awqsomee <awqsome@bk.ru>
2023-10-01 14:54:54 +0300

    fix

--- src/widgets/content-layout/hooks/use-content-layout.tsx
+++ src/widgets/content-layout/hooks/use-content-layout.tsx
@@ -1,6 +1,4 @@
 import useLkNotifications from '@entities/lk-notifications/hooks/use-lk-notifications'
-import { settingsModel } from '@entities/settings'
-import { userModel } from '@entities/user'
 import useCurrentExactPage from '@shared/lib/hooks/use-current-exact-page'
 import useIsShowWhatsNew from '@shared/lib/hooks/use-is-whats-new'
 import useTheme from '@shared/lib/hooks/use-theme'
@@ -9,18 +7,11 @@ import { WhatsNew } from 'widgets'
 import useModal from 'widgets/modal'
 
 const useContentLayout = () => {
-    const {
-        data: { user },
-    } = userModel.selectors.useUser()
     const currentPage = useCurrentExactPage()
 
     const { open } = useModal()
     const isShowWhatsNew = useIsShowWhatsNew()
     // const { seen } = useShowTutorial()
-
-    useEffect(() => {
-        if (user) settingsModel.effects.getLocalSettingsFx(user.id)
-    }, [user])
 
     useLkNotifications()
 
